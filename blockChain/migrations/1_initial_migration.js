5e1f8ca240d3211136d2ee1d016c0442251c3e1c
Alberto156 <lm5708144@gmail.com>
2021-07-04 17:23:18 -0400

    add contract tokens

--- blockChain/migrations/1_initial_migration.js
+++ blockChain/migrations/1_initial_migration.js
@@ -1,5 +1,9 @@
 const Migrations = artifacts.require("Migrations");
+const BabyCoin = artifacts.require("BabyCoin");
+const CatCoin = artifacts.require("CatCoin");
 
 module.exports = function (deployer) {
   deployer.deploy(Migrations);
+  deployer.deploy(BabyCoin ,'BabyCoin', 'BC', 3, 10000000000);
+  deployer.deploy(CatCoin , 'CatCoin', 'CC', 3, 4000000 );
 };
