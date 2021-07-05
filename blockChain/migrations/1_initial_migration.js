eacc7d9461c48f683c897ef2fa48cfc9c31e41ff
Alberto156 <lm5708144@gmail.com>
2021-07-04 23:46:07 -0400

    add contract in account.jsx

--- blockChain/migrations/1_initial_migration.js
+++ blockChain/migrations/1_initial_migration.js
@@ -5,5 +5,5 @@ const CatCoin = artifacts.require("CatCoin");
 module.exports = function (deployer) {
   deployer.deploy(Migrations);
   deployer.deploy(BabyCoin ,'BabyCoin', 'BC', 3, 10000000000);
-  deployer.deploy(CatCoin , 'CatCoin', 'CC', 3, 4000000 );
+  deployer.deploy(CatCoin , 'CatCoin', 'CC', 3, 10000000000);
 };
